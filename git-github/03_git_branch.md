3d9658c983ae2cb7a077ca782343d759ac9e38c6
Matias Benary <matiasbenary@gmail.com>
2020-05-05 10:37:13 -0300

    :pencil: Clase 31 05/05 Comenzamos js

--- git-github/03_git_branch.md
+++ git-github/03_git_branch.md
@@ -5,3 +5,18 @@ git branch nombre -> me crea una rama nueva si no existe,esta es una copia de la
 git checkout nombre -> me para en esa rama
 
 git merge nombre -> unir las ramas,se compara las dos ramas y agrega lo nuevo.
+
+git clone
+git branch footer
+git checkout footer
+/_resuelvo mi tareas _/
+git add -A
+git commit -m "commit"
+git push origin footer(opciona si no se termina)
+git checkout master
+git pull origin master
+git merge footer
+si se genera conflictos resolverlos
+git add -A
+git commit -m "commit"
+git push origin master
---
+++ javascript/Clase diaria/02_variables/app.js
@@ -0,0 +1,39 @@
+/*
+
+var: metodo viejo
+let: varibles que se pueden modificar y tienen un scope reducido.
+const: variables que no se puede modificar,son constante.
+= es asignacion
+
+*/
+
+/*
+nombre cortos y descriptivos
+sin abreviaturas
+lower camel case,esto quiere decir que cada palabra empieza con mayuscula
+*/
+
+// int son numero enteros
+const number = 10;
+const number2 = 15;
+let number3 = 0.2;
+let number4 = 0.3;
+const number5 = number + number2;
+test = 13;
+// string
+const texto = "hola";
+let texto1 = "test";
+// template strings
+let texto2 = `mi numero es ${number}, que genial`;
+let texto3 = "mi numero es " + number + ", que genial";
+
+// boolean
+// siempre en positivo el nombre de valiable
+const isValid = false;
+let isChecked = true;
+// array
+const listaDeCompras = ["pan", "manteca", false, number, [texto, texto1]];
+
+// variables nullas
+const test = null;
+let test2 = undefined;

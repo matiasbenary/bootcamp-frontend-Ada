ad499b0f9f6e3b574dcc8475cc092058340a08a5
Matias Benary <matiasbenary@gmail.com>
2020-05-13 15:09:47 -0300

    :pencil: Clase 37 13/05 Se agrego clase de repaso

--- javascript/Clase diaria/08_array/app.js
+++ javascript/Clase diaria/08_array/app.js
@@ -1,34 +1,43 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Array es un objeto que contiene ninguno o varios elementos
+
+const numeros = [12, 20, 35, 47, 5, 6, 7, 8, 9];
 
 const meses = new Array("ene", "feb", "mar", "abr");
 
 const remix = [1, "soy un simple texto", null, meses, numeros];
+// // El indice es el valor que hace referencia a una variable dentro del array,es decir,es la posicion donde esta el valor - by Vale.
+// console.log(remix);
+// console.log(remix[0]);
+// console.log(remix[3][0]);
+// console.log("largo:", numeros.length);
 
-console.log("largo:", numeros.length);
-console.log("Es un array?", Array.isArray(numeros));
-console.log("meses", meses);
-console.log("enero", meses[0]);
-meses[4] = "may";
-meses.push("jun");
-meses.unshift("PreEnero");
-console.log(meses);
+// for (let i = 0; i < meses.length; i++) {
+//   console.log(meses[i]);
+// }
+// console.log("Es un array?", Array.isArray(numeros));
+// console.log("meses", meses);
+// console.log("enero", meses[0]);
+// meses[4] = "may";
+// meses.push("jun");
+// meses.unshift("PreEnero");
+// console.log(meses);
 
-// Elimina el ultimo elemento del array
-meses.pop();
-// Elimina el primer elemento del array
-meses.shift();
+// // Elimina el ultimo elemento del array
+// meses.pop();
+// // Elimina el primer elemento del array
+// meses.shift();
 
-console.log(meses);
+// console.log(meses);
 
-console.log("Donde esta Marzo:", meses.indexOf("mar"));
-console.log("Saquemos dos elementos,marzo y abril", meses.splice(2, 2));
-console.log("De cabeza", meses.reverse());
-console.log("vamos a unir:", meses.concat(numeros));
-console.log("vamos a ordenar:", meses.sort());
+// console.log("Donde esta Marzo:", meses.indexOf("mar"));
+// console.log("Saquemos dos elementos,marzo y abril", meses.splice(2, 2));
+// console.log("De cabeza", meses.reverse());
+// console.log("vamos a unir:", meses.concat(numeros));
+// console.log("vamos a ordenar:", meses.sort());
 
-const numerosDesordenados = [12, 32, 213, 14, 85, 26, 75, 38, 69];
+// const numerosDesordenados = [12, 32, 213, 14, 85, 26, 75, 38, 69];
 
-console.log(
-  "vamos a ordenar:",
-  numerosDesordenados.sort((x, y) => x - y)
-);
+// console.log(
+//   "vamos a ordenar:",
+//   numerosDesordenados.sort((x, y) => x - y)
+// );
---
+++ javascript/Ejercicios/04_bucles/monstrou/app.js
@@ -0,0 +1,20 @@
+/* EJ monstruo 
+1- definir variables: 
+- Vida jugadora 
+- Vida monstruo 
+- Cantidad de pociones 
+- Ataque máximo jugadora 
+- Ataque máximo monstruo 
+- Curación máxima poción 
+2- escribir cada accion del switch 
+-atacar mosntruo: mathrandom para eegir num e/ 1 y ataque max jugadora restarle el resultado a vida del monstruo 
+-tomar pocion: mathrandom para eegir num e/ 1 y cant max pocion. sumarlo a vida jugadora. restarle 1 a cant pociones 
+-buscar pocion: mathrandom para eegir num e/ 1 y 4 si sale 1 sumar pocion else: nada -salir (cada uno es una funcion) nombrar fx 
+
+3- mostrar los valores de todo (alert, console.log()) 
+4-solicitar al usuario la accion deseada (prompt) 
+5- correr el switch 6- mostrar valores actualizados 
+6 y 1/2 - chequear que los valores no sean cero 
+7- llamar ataque monstruo (fx) 
+8-repetir paso 3 hasta que algun valor llegue a 0 
+9- msj con resultado final */

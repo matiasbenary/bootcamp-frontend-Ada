9238f4bb20bf69462855bd5ee87fb8ee5d54a6fb
Matias Benary <matiasbenary@gmail.com>
2020-05-29 09:23:04 -0300

    :pencil: Ejercicios de Objetos

---
+++ javascript/Ejercicios/06_DOM/tercera parte/toggleChecked/app.js
@@ -0,0 +1,19 @@
+// - Crear un input de tipo checkbox.
+// - Escribir una funcion "toggleChecked" que
+// - cambie el estado de clickeado a sin
+// - clickear o viceversa.
+
+// const listaColeres = ["azul", "rojo"];
+// let indice
+// const secuenciaColores = [];
+// retornar un color azar
+// colorAleaotorio();
+
+// jugarPartida();
+// secuenciaColores.push(colorAleaotorio);
+// mostrar secuenciaColores
+// ingrese los colores botones
+// for(secuenciaColores)
+// if(secuenciaColores[i] === colorIngresado)-> pasa al siguiente
+// sino muestra mensaje perdiste
+//

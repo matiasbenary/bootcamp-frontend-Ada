91af046107822001dc19ee854f9758452f45c052
Matias Benary <matiasbenary@gmail.com>
2020-06-04 09:40:57 -0300

    :pencil: Clase 51 04/06 Teoria de filter y reduce

--- javascript/Clase diaria/14_callbacks/app.js
+++ javascript/Clase diaria/14_callbacks/app.js
@@ -25,3 +25,44 @@ for(let i = 0 ; i< precios.length ; i++){
 const preciosDuplicados = precios.map((precio)=>{
     return precio*1.21
 })
+
+
+// const costos = [12.5, 56, 98, 45.75];
+
+// const agregarIVA = function (costo) {
+//   return costo * 1.21;
+// };
+
+// const sumarGanancia = function (costo) {
+//   return costo * 1.5;
+// };
+
+// // codear acá la solución del ejercicio
+// const preciosFinales = costos.map(agregarIVA).map(sumarGanancia);
+
+// console.log(preciosFinales);
+
+
+// Filter recorrer el array y me deja los elementos que sean verdareros en callback
+const multiplosDe10 = precios.filter((precio)=>{
+    return (precio % 10) === 0;
+})
+console.log("multiplos de 10 son", multiplosDe10);
+
+
+// //  
+// let aux = 0;
+// for (let index = 0; index < array.length; index++) {
+//     aux += array[index];
+    
+// }
+
+// Reduce usa un acumulador,que es una variable que se mantiene en cada ronda
+// esta se pasa por parmetro seguido del valor del array
+// si despues del callback colocamos una coma y colocamos un valor este va ser el valor inicial del acumulador
+// sino va a ser el primer elemento del array 
+const precioTotal = productos.reduce((acumulador,valor) =>{
+    return acumulador + valor;
+},)
+
+console.log("Precio total con reduce",precioTotal);

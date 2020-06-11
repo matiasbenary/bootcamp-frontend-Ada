16142f2c258ae39fa390052d0a67771c5f7ce6e0
Matias Benary <matiasbenary@gmail.com>
2020-06-11 09:33:24 -0300

    :pencil: 11/06 Clase 56 Se agrego repaso de reduce

---
+++ javascript/Clase diaria/repaso/11_06/app.js
@@ -0,0 +1,42 @@
+const nombres = [
+  "Matias",
+  "Maria",
+  "Giselle",
+  "Florencia",
+  "Ezequiel",
+  "Matias",
+  "Maria",
+  "Maria",
+  "Jose",
+  "Jose",
+];
+
+// {
+//   "Matias": 2,
+//   "Maria": 3,
+//   "Giselle":1
+// }
+
+// 1- verifico que tengo retornar,esto va ser mi acumulador
+// 2- es matener mi acumulador en este formato
+// 3- siempre debo retornar el acumulador
+const cantidadNombres = nombres.reduce((contadorNombre, nombre, indice) => {
+  //  {},"Matias",0
+  //  {Matias:1},"Maria",1
+  //
+  //
+  //
+  console.log(contadorNombre, nombre, indice, contadorNombre[nombre]);
+  if (contadorNombre[nombre]) {
+    contadorNombre[nombre]++;
+  } else {
+    contadorNombre[nombre] = 1;
+  }
+  return contadorNombre;
+}, {});
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const numChico = numeros.reduce((minNum, numero) =>
+  minNum > numero ? minNum : numero
+);
